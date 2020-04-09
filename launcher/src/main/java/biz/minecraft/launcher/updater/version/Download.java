0acac0941056ef3f173b7cca04806f4b83988c06
Maxim Pesotskiy <defilak@mail.ru>
2020-04-09 23:43:00 +0300

    fix

--- launcher/src/main/java/biz/minecraft/launcher/updater/version/Download.java
+++ launcher/src/main/java/biz/minecraft/launcher/updater/version/Download.java
@@ -1,20 +1,30 @@
 package biz.minecraft.launcher.updater.version;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Path;
+
+import javax.swing.JProgressBar;
+import javax.swing.SwingUtilities;
+
+import org.apache.commons.io.input.AutoCloseInputStream;
 
 import biz.minecraft.launcher.Util;
 
 public class Download {
 
     private URL url;
-    private Path path; 
+    private String path;
 
-    public Download() { }
+    public Download() {
+    }
 
     public Download(URL url, Path path) {
         this.url = url;
-        this.path = path;
+        this.path = path.toString();
     }
 
     public Download(String url, String path) {
@@ -26,11 +36,40 @@ public class Download {
     }
 
     public Path getPath() {
-        return path;
+        return Path.of(this.path);
     }
 
     public Download setPathParent(String parent) {
-        this.path = Path.of(parent).resolve(this.path);
+        this.path = parent + this.path;
         return this;
+    }
+
+    public void download(final JProgressBar jProgressBar) throws IOException, InterruptedException {
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        int completeFileSize = connection.getContentLength();
+        int downloadedFileSize = 0;
+
+        jProgressBar.setMaximum(completeFileSize);
+
+        AutoCloseInputStream in = new AutoCloseInputStream(connection.getInputStream());
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(this.getPath().toFile()));
+
+        byte data[] = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = in.read(data, 0, 1024)) != -1) {
+
+            downloadedFileSize += bytesRead;
+            // calculate progress
+            final int currentProgress = (int) ((((double) downloadedFileSize) / ((double) completeFileSize)) * 100000d);
+            // update progress bar
+            final int value = downloadedFileSize;
+            SwingUtilities.invokeLater(() -> jProgressBar.setValue(value));
+            out.write(data, 0, bytesRead);
+            Thread.sleep(1);
+        }
+
+        out.close();
+        System.out.println("Downloading: " + path + " From: " + url);
     }
 }
